84e91b658d1014c3e1b822eb4bc48c9211f433e6
Charawey-X <charawey@gmail.com>
2023-01-17 10:42:58 +0300

    create users with non-null values & unique id and email

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,10 @@
 --id is a unique integer; primary key
 --id and email are non-null
 --email and name are strings of 255 characters
-CREATE TABLE IF NOT EXISTS users 
-(id INT NOT NULL AUTO_INCREMENT, 
-email VARCHAR(255) NOT NULL UNIQUE, 
-name VARCHAR(255), 
-PRIMARY KEY(id));
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT, 
+	email VARCHAR(255) NOT NULL UNIQUE, 
+	name VARCHAR(255), 
+	PRIMARY KEY(id)
+);
