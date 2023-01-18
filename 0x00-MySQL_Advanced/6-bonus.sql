adf7097bc38334ab036af2ce2824d5c939299e50
Charawey-X <charawey@gmail.com>
2023-01-18 12:47:00 +0300

    create a stored procedure AddBonus that adds a new correction for a student

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,15 +1,15 @@
 -- creates a stored procedure AddBonus that adds a new correction for a student
 
 DELIMITER //
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN 
-	DECLARE @project_id INT;
+	DECLARE project_id INT;
 	IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 
 	THEN 
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
-	SET @project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+	SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
 	INSERT INTO corrections (user_id, project_id, score) 
-	VALUES (user_id, @project_id, score);
+	VALUES (user_id, project_id, score);
 END //
 DELIMITER;
