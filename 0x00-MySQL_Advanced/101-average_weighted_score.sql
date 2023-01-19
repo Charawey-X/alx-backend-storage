6db72593b5eebecb0c4fa28bcc196623e0566651
Charawey-X <charawey@gmail.com>
2023-01-19 11:44:30 +0300

    create a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for all students

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
 	UPDATE users AS U, 
 	(
-		SELECT U.id, SUM(score * weight)/SUM(weight) AS WA FROM users AS U,
-		JOIN corrections AS C ON C.user_id = U.id
-		JOIN projects AS P ON P.id = C.project_id
+		SELECT U.id, SUM(score * weight)/SUM(weight) AS w_avg FROM users AS U,
+		JOIN corrections AS C ON U.id = C.user_id
+		JOIN projects AS P ON C.project_id = P.id
 		GROUP BY U.id
-	) AS weighted_averages 
-	SET U.average_score = weighted_averages.WA WHERE U.id = weighted_averages.id;
+	) AS WA 
+	SET U.average_score = WA.w_avg WHERE U.id = WA.id;
 END //
 DELIMITER ;
